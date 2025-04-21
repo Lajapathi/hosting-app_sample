9223498f459815e3ffa068918910dacb71b5ccbf
Lajapathidurai R A <151938474+Lajapathi@users.noreply.github.com>
2025-04-21 07:26:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import pandas as pd
+#import pandas as pd
 
 #pip3 install -r requirements.txt
 #streamlit run main.py
 
-df=pd.read_csv('/Users/apple/Documents/Guvi/AWS Clould/hosting_app/crop_production_prime_new-2.csv')
-show_df=df.head()
+# df=pd.read_csv('/Users/apple/Documents/Guvi/AWS Clould/hosting_app/crop_production_prime_new-2.csv')
+# show_df=df.head()
 st.title("Sample for hosting")
 st.write("Ellie How are you")
-st.dataframe(show_df)
+#st.dataframe(show_df)
