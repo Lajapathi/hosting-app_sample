2fd9f1b84efbedea22d15b0596c07aa179dcf7be
Lajapathidurai R A <151938474+Lajapathi@users.noreply.github.com>
2025-04-21 09:19:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pandas as pd
 #pip3 install -r requirements.txt
 #streamlit run main.py
 
-#df=pd.read_csv('/Users/apple/Documents/Guvi/AWS Clould/hosting_app/crop_production_prime_new-2.csv')
-#show_df=df.head()
+df=pd.read_csv('crop_production_prime_new-2.csv')
+show_df=df.head()
 st.title("Sample for hosting")
 st.write("Ellie How are you")
-#st.dataframe(show_df)
+st.dataframe(show_df)
